c3f5ed5eb6714d856a85ed4e50cb6e5de49577d3
Sara Nolen <nolensara1@gmail.com>
2020-11-05 23:54:58 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,6 @@
+colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
 def display_rainbow(colors)
   puts display_rainbow #{colors}
 
 end
-colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+
